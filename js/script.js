d960ffa9c45eebefb6f0d60674c160e4c35ecacb
oviyasenthilkumar <154113657+oviyasenthilkumar@users.noreply.github.com>
2024-02-21 13:47:06 +0530

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,7 +1,6 @@
 let signUpForm = document.getElementById("signup");
 signUpForm.addEventListener("submit", signUpRegistration);
 
-
 let signUpBtn = document.getElementById("signUpBtn");
 signUpBtn.addEventListener("click",function commit(){
     signUpForm.classList.remove("hidden")
@@ -17,21 +16,28 @@ event.preventDefault();
 let username = document.querySelector("#createUsername").value;
 let password = document.querySelector("#createPassword").value;
 console.log(username, password);
-// get data from local storage
-let users = JSON.parse(localStorage.getItem("users")) || [];
-console.log(users);
-//check duplicate data
-let existingUser = users.find((user)=> user.username === username);
-console.log(existingUser);
-if(existingUser){
-    alert("User already exists. please create a new one");
+if(password > 8 && password < 15){  
+    if((password > a && password < z)&&(password > A && password < Z)){
+        return alert("your password successfully created");
+    }
+    // get data from local storage
+    let users = JSON.parse(localStorage.getItem("users")) || [];
+    console.log(users);
+    //check duplicate data
+    let existingUser = users.find((user)=> user.username === username);
+    console.log(existingUser);
+    if(existingUser){
+        alert("User already exists. please create a new one");
+    }else{
+        psw(password);
+        users.push({username:username, password:password})
+    //update username and password
+
+    localStorage.setItem("users", JSON.stringify(users));
+    alert("Registration updated successfully")
+    }
 }else{
-    users.push({username:username, password:password})
-//update username and password
-
-localStorage.setItem("users", JSON.stringify(users));
-alert("Registration updated successfully")
-
+    alert("reset ur password with one uppercase,one lowercase and password must contain 8 - 15 characters");
 }
 }
 
